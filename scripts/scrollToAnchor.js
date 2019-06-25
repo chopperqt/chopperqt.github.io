b8415b14be065fc07333cdd15081f95caa483fd6
Chopper <ilua292010@gmail.com>
2019-06-25 22:16:11 +0300

    1.0

--- scripts/scrollToAnchor.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("select").change(function (event) {
-        event.preventDefault();
-        let id = $("select option:selected").attr('href'),top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1000);
-        console.log(id);
-    })
-});
