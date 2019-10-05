371ee9367868d9629c1e011477009449fe71927d
Chopper <ilua292010@gmail.com>
2019-10-05 18:07:29 +0300

    Tic-Tac-Toe

---
+++ tic-tac-toe/precache-manifest.1541d8cc35e3017f0039750b48f82c10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dfc0bf01599d7f4779c8293c7b12b4b",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "4f315735f9baf37b16fe",
+    "url": "/tic-tac-toe/static/css/main.3df56eaf.chunk.css"
+  },
+  {
+    "revision": "c4453a69d2b38d30d13b",
+    "url": "/tic-tac-toe/static/js/2.62210ec3.chunk.js"
+  },
+  {
+    "revision": "4f315735f9baf37b16fe",
+    "url": "/tic-tac-toe/static/js/main.46f0b8d4.chunk.js"
+  },
+  {
+    "revision": "5416bbc324f4b21bf15f",
+    "url": "/tic-tac-toe/static/js/runtime-main.4dc36861.js"
+  }
+]);
