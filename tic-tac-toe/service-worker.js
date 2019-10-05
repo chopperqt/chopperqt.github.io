f7ab4ff6198c706d87743c2255f353edb12b182f
Chopper <ilua292010@gmail.com>
2019-10-05 18:15:26 +0300

    Tic-Tac-Toe

--- tic-tac-toe/service-worker.js
+++ tic-tac-toe/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eda11800a09d37aa023affb8cad50680.js"
+  "/tic-tac-toe/precache-manifest.1541d8cc35e3017f0039750b48f82c10.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac-toe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
